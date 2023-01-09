87ec6ce441100f899727981963bd5d3bf28d850a
gurjot74 <gagan.singh@tothenew.com>
2023-01-09 13:08:38 +0530

    fixed bug in venue info api

--- src/controllers/venue-info-controller.js
+++ src/controllers/venue-info-controller.js
@@ -17,7 +17,8 @@ module.exports.getVenueInfo = async (req, res) => {
   try {
     const { venueId } = req.params;
     const result = await getVenueInfo(+venueId);
-    return res.send(200, { data: result, venueDetails: { venueName: result[0]?.venueName, venueState: result[0]?.venueState, venueType: result[0]?.venueType, latitude: result[0]?.latitude, longitude: result[0]?.longitude } });
+    console.log(result)
+    return res.send(200, { data: result[0].venueInfo, venueDetails: { venueName: result[0].venueInfo[0]?.venueName, venueState: result[0].venueInfo[0]?.venueState, venueType: result[0].venueInfo[0]?.venueType, latitude: result[0].venueInfo[0]?.latitude, longitude: result[0].venueInfo[0]?.longitude }, active_users: result[0].activeUsers[0]?.active_users });
   } catch (err) {
     throw err;
   }
