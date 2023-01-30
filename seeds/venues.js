9c425787fb50f9ae2fdbe00ad14285ec4a17c49d
Manoj Nama <manoj.nama@outlook.com>
2023-01-30 11:15:29 +0530

    Fix venue seedinf

--- seeds/venues.js
+++ seeds/venues.js
@@ -6,7 +6,7 @@ const VENUE_LIST_API = 'https://api.congo.beta.tab.com.au/v1/invenue-service/pub
 const seedVenues = async () => {
   console.log("Seeding venues");
   const redisClient = redis.getRedis();
-  let venues = await redisClient.get('venues');
+  let venues = await redisClient.get('venues') || [];
   if (venues.length) {
     console.log("Venues already seeded!");
     return;
